4115e106aa37090be5aaffffb4a152da0f1fa148
Alex <archmon@hotmail.com>
2021-04-23 02:00:14 -0400

    Basic form with basic state

--- src/frontend/goose/src/App.js
+++ src/frontend/goose/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React from "react";
+import { useState } from "react";
+import axios from 'axios'
+
+
 import './App.css';
 
 function App() {
+  const [selectedFile, setSelectedFile] = useState('')
+
+  const fileSelectedHandler = event => {
+    setSelectedFile(event)
+  }
+
+  const fileUploadHandler = (event) => {
+    
+    console.log(selectedFile.target.files[0])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <div>
+        <input type="file" onChange={fileSelectedHandler}/>
+        <button onClick={fileUploadHandler}>Upload</button>
+      </div>
   );
 }
 
 export default App;
+
